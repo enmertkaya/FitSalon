c54c6207871e4c289f2bcf54198a02aaad2cd848
Mert Kaya <merteneskaya outlook.com>
2024-08-21 20:23:37 +0300

    Add project files.

---
+++ FitSalon.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitSalon.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
