460890eeb61b8de6b5dac2934b2f84e9a4b4236f
Enes Mert Kaya <merteneskaya@outlook.com>
2024-08-26 21:10:25 +0300

    default , service controller eklendi

---
+++ FitSalon.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitSalon.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FitSalon.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,35 @@
+﻿using FitSalon.BusinessLayer.Concrete;
+using FitSalon.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using FitSalon.BusinessLayer.Abstract;
+using FitSalon.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNet.Identity;
+
+namespace FitSalon.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EFServiceDal());
+
+        public IActionResult Index()
+        {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ServiceDetails(int id)
+        {
+       
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ServiceDetails(Service p)
+        {
+            return View();
+        }
+    }
+}
