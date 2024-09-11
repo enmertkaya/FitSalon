ede315061e60781da45f1a8b32b44cf9c01e3780
Enes Mert Kaya <merteneskaya@outlook.com>
2024-09-11 15:09:53 +0300

    visitor api

---
+++ FitSalon.WebUI/Controllers/VisitorApiController.cs
@@ -0,0 +1,30 @@
+﻿using FitSalon.WebUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FitSalon.WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class VisitorApiController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public VisitorApiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7251/api/Visitor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<VisitorViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ FitSalon.WebUI/Models/VisitorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FitSalon.WebUI.Models
+{
+    public class VisitorViewModel
+    {
+        public int VisitorID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
