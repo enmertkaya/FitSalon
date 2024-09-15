e5e4a14a41abb18ba76e6042d79ee8ee85e27baf
Enes Mert Kaya <merteneskaya@outlook.com>
2024-09-15 18:03:22 +0300

    role atama islemleri

--- FitSalon.WebUI/Areas/Admin/Controllers/ExcelController.cs
+++ FitSalon.WebUI/Areas/Admin/Controllers/ExcelController.cs
@@ -8,8 +8,8 @@ using FitSalon.BusinessLayer.Abstract;
 
 namespace FitSalon.WebUI.Areas.Admin.Controllers
 {
-    [AllowAnonymous]
     [Area("Admin")]
+    [AllowAnonymous]
 
     public class ExcelController : Controller
     {
@@ -27,50 +27,51 @@ namespace FitSalon.WebUI.Areas.Admin.Controllers
 
         public List<ServiceModel> ServiceList()
         {
-            List<ServiceModel> _serviceModels = new List<ServiceModel>();
+            List<ServiceModel> serviceModels = new List<ServiceModel>();
             using (var c = new Context())
             {
-                _serviceModels = c.Services.Select(x => new ServiceModel
+                serviceModels = c.Services.Select(x => new ServiceModel
                 {
                     City = x.City,
                     DayNight = x.DayNight,
                     Price = x.Price,
                     Capacity = x.Capacity
                 }).ToList();
-                return _serviceModels;
             }
+            return serviceModels;
         }
 
         public IActionResult StaticExcelReport()
         {
             return File(_excelService.ExcelList(ServiceList()), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniExcel.xlsx");
+            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
         }
 
         public IActionResult ServiceExcelReport()
         {
-            using (var workbook = new XLWorkbook())
+            using (var workBook = new XLWorkbook())
             {
-                var worksheet = workbook.Worksheets.Add("İşletme Listesi");
-                worksheet.Cell(1, 1).Value = "Şehir";
-                worksheet.Cell(1, 2).Value = "Süre";
-                worksheet.Cell(1, 3).Value = "Fiyat";
-                worksheet.Cell(1, 4).Value = "Kapasite";
+                var workSheet = workBook.Worksheets.Add("Firma Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Paket Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kapasite";
 
                 int rowCount = 2;
                 foreach (var item in ServiceList())
                 {
-                    worksheet.Cell(rowCount, 1).Value = item.City;
-                    worksheet.Cell(rowCount, 2).Value = item.DayNight;
-                    worksheet.Cell(rowCount, 3).Value = item.Price;
-                    worksheet.Cell(rowCount, 4).Value = item.Capacity;
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 2).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 3).Value = item.Price;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
                     rowCount++;
                 }
 
                 using (var stream = new MemoryStream())
                 {
-                    workbook.SaveAs(stream);
+                    workBook.SaveAs(stream);
                     var content = stream.ToArray();
-                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DinamikRotaListesi.xlsx");
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniListe.xlsx");
                 }
             }
         }
